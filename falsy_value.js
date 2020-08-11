fbb65a273585fae5c7f3bff932b8d7bcff9c6657
HSabbir <43398598+HSabbir@users.noreply.github.com>
2020-08-11 05:45:40 -0700

    falsy  done

---
+++ falsy_value.js
@@ -0,0 +1,17 @@
+// falsy
+// undefined
+// null
+// 0
+// NaN
+
+// var user = null;
+//
+// if(user){
+//     console.log("Working");
+// }
+
+var user = "2";
+
+if(2 === user){
+    console.log("Working");
+}
