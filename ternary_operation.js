d11afb05a50394a936b8f958f4651687e93a4018
HSabbir <43398598+HSabbir@users.noreply.github.com>
2020-08-11 05:17:15 -0700

    ternary operation done

---
+++ ternary_operation.js
@@ -0,0 +1,11 @@
+// check user and show login/signup option
+
+var authenticated = false;
+
+// if(authenticated){
+//     console.log("Show Sign Out Button");
+// }else{
+//     console.log("Show Sign In or login Button");
+// }
+
+authenticated ? console.log("Show Sign Out Button") : console.log("Show Sign In or login Button") ;
