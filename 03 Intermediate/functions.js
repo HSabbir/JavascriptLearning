de8f2fa93bff161e0dcfb40b40ff2dca25de2e2d
HSabbir <43398598+HSabbir@users.noreply.github.com>
2020-08-11 08:48:56 -0700

    function started

---
+++ 03 Intermediate/functions.js
@@ -0,0 +1,11 @@
+function Hello(name) {
+    console.log(`Hello, ${name}`);
+}
+
+function returnHellow(name){
+    return `hellow ${name}`;
+}
+
+Hello("Sabbir");
+
+console.log(returnHellow("roki vai"));
